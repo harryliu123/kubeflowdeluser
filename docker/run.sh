3d4bfd973af4610f0e2dff72689f7f0803525d39
harryliu123 <harryliu123@users.noreply.github.com>
2020-07-14 10:43:15 +0800

    過濾dex local

--- docker/run.sh
+++ docker/run.sh
@@ -5,24 +5,36 @@
 #basednpw="密碼"
 #bindDN="ou=example,dc=example,dc=com"  ## 搜尋的位置
 
+## 確定LDAP 連線正常
 ldapstatus=$(ldapsearch -x -h $ldaphost -D $basedn -p 389 -w $basednpw |grep numResponses)
-name=$(kubectl get profile -o json | jq '.items' |jq .[].spec.owner.name |grep -v admin| awk -F'"' '{print $2}'| awk -F'@' '{print $1}')
-array=( $name )
-for i in "${array[@]}"
+
+## 擷取profile 的email欄位
+email=$(kubectl get profile -o json | jq '.items' |jq .[].spec.owner.name | awk -F'"' '{print $2}')
+arrayemail=( $email )
+
+## 擷取dex lcoal 設定的email欄位
+dexlocalaccount=$(kubectl get configmap dex -n auth -o jsonpath='{.data.config\.yaml}'|grep email |awk -F': ' '{print $2}')
+
+## 比對 profile email 是否存在於 dex lcoal 設定的email欄位 && profile name 是否存在於 ldap 的帳號
+## 比對不到代表 ldap 已經刪除的帳號, 進行profile 刪除
+for i in "${arrayemail[@]}"
 do
-    status=$(ldapsearch \
+    name=$(echo $i| awk -F'@' '{print $1}')
+    echo $name
+    dexlcoalstatus=$(echo $dexlocalaccount |grep $name)
+    ldapnamestatus=$(ldapsearch \
         -x -h $ldaphost \
         -D $basedn \
         -p 389 \
         -w $basednpw \
-        -b "$bindDN" "cn=$i" \
+        -b "$bindDN" "cn=$name" \
         -s sub "(cn=*)" cn | grep numEntries)
-    echo $i
-    if [ -z "$status" ] && [ ! -z "$ldapstatus" ]
+
+    if [ -z "$ldapnamestatus" ] && [ ! -z "$ldapstatus" ] && [ -z "$dexlcoalstatus" ]
     then
-          echo "delete user $i"
-          kubectl delete profile $i
+          echo "delete user $name"
+          kubectl delete profile $name
     else
-          echo "Ldap have user nothing"
+          echo "Ldap/dex_local have user nothing"
     fi
 done
